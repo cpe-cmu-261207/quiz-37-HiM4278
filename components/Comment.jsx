c210b489fdf34a44859938f46cdfd768ddf79d90
pasakorn <pasakorn141@gmail.com>
2022-09-15 14:29:28 +0700

    completed

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -2,12 +2,49 @@ import React from "react";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const reply = props.replieuser;
+
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.userimg}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.comText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <span>
+              {props.like <= 0 ? false : <img src="/like.svg" width={20}></img>}
+            </span>
+            {props.like <= 0 ? (
+              false
+            ) : (
+              <span className="text-muted">{props.like} คน</span>
+            )}
+          </div>
+        </div>
+      </div>
 
       {/*render Reply here... */}
+      {reply.map((x, i) => (
+        <Reply
+          repliename={x.username}
+          replieText={x.replyText}
+          like={x.likeNum}
+          userimg={x.userImagePath}
+          key={i}
+        />
+      ))}
     </div>
   );
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="d-flex gap-2 my-2 ps-5">
+        <img
+          src={props.userimg}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.repliename}</span>
+          <br />
+          <span>{props.replieText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <span>
+              {props.like <= 0 ? false : <img src="/like.svg" width={20}></img>}
+            </span>
+            {props.like <= 0 ? (
+              false
+            ) : (
+              <span className="text-muted">{props.like} คน</span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
